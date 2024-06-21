4229086a51529ff8209345afb593ba87f589011b
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-06-21 11:24:23 +0545

    Add about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Intro from './components/Intro.jsx'
+import About from './components/About.jsx'
 
 function App() {
 	return (
 		<>
 			<Intro/>
+			<About/>
 		</>
 	)
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,18 @@
+function About() {
+	return (
+		<>
+			<h1>About Me</h1>
+			
+			<div className='about-content'>
+				<div className='about-description'>
+					I’m currently pursuing a BSc (H) in Computing degree at The British College, Kathmandu. As an aspiring front-end developer and software enthusiast, I’m passionate about creating intuitive user experiences. I also enjoy developing games as a hobby. Beyond my technical interests, I’ve gained professional experience as a Data Analyst and a Bench Sales Recruiter.
+				</div>
+				<div className='about-image'>
+					<img src={'/assets/me2.jpg'} alt='Bhabishwor Gurung' />
+				</div>
+			</div>
+		</>
+	);
+}
+
+export default About;
