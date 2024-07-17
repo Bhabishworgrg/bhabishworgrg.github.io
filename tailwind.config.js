93f15b35d1e0066e32a93ce0443f4ba3fb4fb307
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-07-17 19:24:21 +0545

    Customize tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		extend: {
+			colors: {
+				bgColor: '#1E1E2E', // Background color
+				priColor: '#EAEAEA', // Text color
+				secColor: '#7F5AF0', // Secondary color (hover effects)
+			},
+		},
+	},
+	plugins: [],
 }
