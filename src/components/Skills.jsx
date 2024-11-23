0e368b9c9608980a5863c5d0fd121a1901bb2ed2
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-11-23 20:48:12 +0545

    Stylize skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,29 +1,35 @@
 function Skills() {  
-	function addSkill(title, filename) {
-		return (
-			<>
-				<img src={ `/assets/${ filename }.png` } alt={ filename }/>
-				<span>{ title }</span>
-			</>
-		);
-	};
+	const items = [
+		{ image: '/assets/html_css.png', label: 'HTML/CSS' },
+		{ image: '/assets/js.png', label: 'JavaScript' },
+		{ image: '/assets/react.png', label: 'React' },
+		{ image: '/assets/java.png', label: 'Java' },
+		{ image: '/assets/csharp.png', label: 'C#' },
+		{ image: '/assets/c.png', label: 'C' },
+		{ image: '/assets/python.png', label: 'Python' },
+		{ image: '/assets/godot.png', label: 'Godot' },
+		{ image: '/assets/git.png', label: 'Git' },
+		{ image: '/assets/linux.png', label: 'Linux' },
+	];
 
 	return (
 		<>
 			<h1 id='skills'>Skills</h1>
 
-			<div>
-				{ addSkill('HTML/CSS', 'html_css') }
-				{ addSkill('JavaScript', 'js') }
-				{ addSkill('React', 'react') }
-				{ addSkill('Java', 'java') }
-				{ addSkill('C#', 'csharp') }
-				{ addSkill('C', 'c') }
-				{ addSkill('Python', 'python') }
-				{ addSkill('Godot', 'godot') }
-				{ addSkill('Git', 'git') }
-				{ addSkill('Linux', 'linux') }
-			</div>
+    		<div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 pt-4'>
+      			{ items.map((item) => (
+        			<div
+          			className='flex flex-col items-center bg-secondary p-4'
+        			>
+          				<img
+            			src={ item.image }
+            			alt={ item.label }
+            			className='w-24 h-24 object-cover mb-2 hover:scale-125 transform transition-transform hover:saturate-150'
+          				/>
+          				<span className='text-tertiary'>{item.label}</span>
+        			</div>
+      			))}
+    		</div>
 		</>
 	);
 };
