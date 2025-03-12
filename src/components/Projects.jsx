b0628f93add26b3bbd7eba3ac70939fdf32eae95
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-12 23:45:32 +0545

    Modify projects for mobile view

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -107,15 +107,18 @@ function Projects() {
 
 	const [itemsPerPage, setItemsPerPage] = useState(4);
 	const [currentPage, setCurrentPage] = useState(0);
+	const [isMobile, setIsMobile] = useState(false);
 
 	useEffect(() => {
 		const updateItemsPerPage = () => {
-			setItemsPerPage(window.innerWidth < 640 ? 1 : 4);
+			const mobileView = window.innerWidth < 640;
+			setItemsPerPage(mobileView ? 1 : 4);
+			setIsMobile(mobileView);
 		};
 
 		updateItemsPerPage();
-		window.addEventListener("resize", updateItemsPerPage);
-		return () => window.removeEventListener("resize", updateItemsPerPage);
+		window.addEventListener('resize', updateItemsPerPage);
+		return () => window.removeEventListener('resize', updateItemsPerPage);
 	}, []);
 
 	const totalPages = Math.ceil(items.length / itemsPerPage);
@@ -125,50 +128,50 @@ function Projects() {
 
 	return (
 		<>
-			<h1 id="projects">Projects</h1>
+			<h1 id='projects'>Projects</h1>
 
-			<div className="relative flex flex-col items-center mt-8">
+			<div className='relative flex flex-col items-center mt-8'>
 				<button
 					onClick={prevSlide}
-					className="absolute top-1/2 left-5 text-tertiary hover:text-highlight hover:scale-150 transition-transform">
+					className='absolute top-1/2 left-5 text-tertiary hover:text-highlight hover:scale-150 transition-transform z-10'>
 					❮
 				</button>
 
-				<AnimatePresence mode="wait">
+				<AnimatePresence mode='wait'>
 					<motion.div
 						key={ currentPage }
 						initial={{ opacity: 0, x: 50 }}
 						animate={{ opacity: 1, x: 0 }}
 						exit={{ opacity: 0, x: -50 }}
 						transition={{ duration: 0.4 }}
-						className={ `grid ${ itemsPerPage === 1 ? "grid-cols-1" : "grid-cols-2" } gap-4` }
+						className={ `grid ${ isMobile ? 'grid-cols-1 w-full' : 'grid-cols-2' } gap-4` }
 					>
 						{ items.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage).map((item, index) => (
-							<div key={ index } className="bg-secondary p-6 text-center">
-								<h3 className="text-xl font-semibold">
+							<div key={ index } className={ `bg-secondary p-6 text-center ${ isMobile ? 'w-full' : '' } `}>
+								<h3 className='text-xl font-semibold'>
 									{ item.name }
 								</h3>
-								<p className="text-base">
+								<p className='text-base'>
 									{ item.description }
 								</p>
-								<p className="text-highlight mt-2 text-base">
+								<p className='text-highlight mt-2 text-base'>
 									<em>{ item.techstack }</em>
 								</p>
-								<div className="flex justify-center mt-4 gap-6">
+								<div className='flex justify-center mt-4 gap-6'>
 									<a
 										href={ `https://github.com/Bhabishworgrg/${ item.repo }` }
-										target="_blank"
-										className="hover:text-highlight hover:scale-150 transition-transform"
+										target='_blank'
+										className='hover:text-highlight hover:scale-150 transition-transform'
 									>
-										<FaGithub size="24" />
+										<FaGithub size='24' />
 									</a>
 									{ item.link &&
 										<a
 											href={ item.link }
-											target="_blank"
-											className="hover:text-highlight hover:scale-150 transition-transform"
+											target='_blank'
+											className='hover:text-highlight hover:scale-150 transition-transform'
 										>
-											<FaLink size="24" />
+											<FaLink size='24' />
 										</a>
 									}
 								</div>
@@ -179,15 +182,15 @@ function Projects() {
 
 				<button
 					onClick={nextSlide}
-					className="absolute top-1/2 right-5 text-tertiary hover:text-highlight hover:scale-150 transition-transform">
+					className='absolute top-1/2 right-5 text-tertiary hover:text-highlight hover:scale-150 transition-transform'>
 					❯
 				</button>
 
-				<div className="flex gap-2 mt-6">
+				<div className='flex gap-2 mt-6'>
 					{ Array.from({ length: totalPages }).map((_, index) => (
 						<div
 							key={ index }
-							className={ `h-2 w-2 ${ index === currentPage ? "bg-highlight" : "bg-tertiary" }` }
+							className={ `h-2 w-2 ${ index === currentPage ? 'bg-highlight' : 'bg-tertiary' }` }
 						/>
 					))}
 				</div>
