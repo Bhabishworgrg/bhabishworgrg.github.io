e96edfbd8227af32fc6a616a86f179b68b924d21
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-03-13 00:56:18 +0545

    Update about me's style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,16 +2,23 @@ function About() {
 	return (
 		<>
 			<h1 id='about' className='mt-0'>About Me</h1>
-			
+
 			<div className='flex flex-col lg:flex-row items-center justify-center'>
-				<p className='mt-4 lg:mt-0'>
-I’m currently pursuing a BSc (H) in Computing degree at The British College, Kathmandu. As an aspiring front-end developer and software enthusiast, I’m passionate about creating intuitive user experiences. My fascination for programming extends to building desktop apps and CLI tools as well. I also enjoy developing games as a hobby. Beyond my technical interests, I’ve gained professional experience as a Data Analyst and a Bench Sales Recruiter.
-				</p>
+				<div>
+					<p className='mt-4 lg:mt-0 text-base lg:text-lg'>
+						As an aspiring backend developer and software enthusiast, I am constantly learning about building scalable and efficient web applications. I am currently an undergraduate student of BSc (Hons) Computing at <a href='https://www.thebritishcollege.edu.np/' className='text-highlight'>The British College</a>, Kathmandu.
+					</p>
+					<br/>
+					<p className='text-base lg:text-lg'>
+						My fascination for programming extends to building desktop apps and CLI tools as well. I also make games.
+					</p>
+				</div>	
 				
 				<img
-				src={ '/me.png' }
-				alt='img: me.png'
-				className='rounded-tl-3xl rounded-br-3xl border-highlight border-4 lg:ml-16 mt-16 lg:mt-0 w-[230px] sm:w-[270px] md:w-[300px] lg:w-[250px] h-auto'/>
+					src={ '/me.png' }
+					alt='img: me.png'
+					className='shadow-lg shadow-highlight lg:ml-16 mt-16 lg:mt-0 w-[230px] sm:w-[270px] md:w-[300px] lg:w-[250px] h-auto'
+				/>
 			</div>
 		</>
 	);
