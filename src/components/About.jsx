1178562c4b441a9f7af48965d122cad5e463715e
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-08-19 00:14:08 +0545

    Update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
 			<div className='flex flex-col lg:flex-row items-center justify-center'>
 				<div>
 					<p className='mt-4 lg:mt-0 text-base lg:text-lg'>
-						As an aspiring backend developer and software enthusiast, I am constantly learning about building scalable and efficient web applications. I am currently an undergraduate student of BSc (Hons) Computing at <a href='https://www.thebritishcollege.edu.np/' className='text-highlight hover:saturate-150 transition-transform'>The British College</a>, Kathmandu.
+						As an aspiring backend developer and software enthusiast, I am constantly learning about building scalable and efficient web applications. I graduated from <a href='https://www.leedsbeckett.ac.uk/' className='text-highlight hover:saturate-150 transition-transform'>Leeds Beckett University</a> with a BSc (Hons) Computing degree in 2025.
 					</p>
 					<br/>
 					<p className='text-base lg:text-lg'>
